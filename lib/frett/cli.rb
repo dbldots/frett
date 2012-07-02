47f1699d7385750ab4c000c8089de2809e11ee65
johannes goetzinger <johannes@johanness-MacBook-Air.local>
2012-07-02 15:40:07 +0200

    extend cli functionality by implementing parameter handling

--- lib/frett/cli.rb
+++ lib/frett/cli.rb
@@ -3,35 +3,47 @@ require 'optparse'
 module Frett
   class CLI
     def self.execute(stdout, arguments=[])
-      needle = arguments.join(" ")
-      Frett::Search.new.search(needle)
-      exit
-
-      mandatory_options = %w(  )
       options = {}
-      parser = OptionParser.new do |opts|
-        opts.banner = <<-BANNER.gsub(/^          /,'')
-          Utility to build search index of a project
-          Usage: #{File.basename($0)} [options]
 
-          Options are:
-        BANNER
-        opts.separator ""
-        opts.on("-p", "--path PATH", String,
-                "This is a sample message.",
-                "For multiple lines, add more strings.",
-                "Default: ~") { |arg| options[:path] = arg }
-        opts.on("-h", "--help",
-                "Show this help message.") { stdout.puts opts; exit }
-        opts.on("init", "--help",
-                "init task.") { puts "INIT"; exit }
-        opts.parse!(arguments)
+      optparse = OptionParser.new do|opts|
+        opts.banner = "Usage: frett [options] 'search string'"
+
+        options[:escape] = nil
+        opts.on( '-n', '--escape', 'escape special characters' ) do
+          options[:escape] = true
+        end
+        opts.on( '-N', '--no-escape', 
+                 'don\'t escape special chars. you may want to use:',
+                 '\'?\' for any character, \'*\' for multiple characters' ) do
+          options[:escape] = false
+        end
+
+        options[:use_wildcard] = nil
+        opts.on( '-w', '--use-wildcard', 'adds a \'*\' in front & to the end of your search string' ) do
+          options[:use_wildcard] = true
+        end
+        opts.on( '-W', '--no-wildcard', 'don\'t wildcard the search string' ) do
+          options[:use_wildcard] = false
+        end
+
+        options[:use_or] = nil
+        opts.on( '-o', '--use-or', 'build search query using OR for the terms of your search string' ) do
+          options[:use_or] = true
+        end
+        opts.on( '-a', '--use-and', 'build search query using AND for the terms of your search string' ) do
+          options[:use_or] = false
+        end
+
+        opts.on( '-h', '--help', 'Display this screen' ) do
+          puts opts
+          exit
+        end
       end
 
-      path = options[:path]
-
-      # do stuff
-      stdout.puts "go fuck yourself"
+      optparse.parse!(arguments)
+      search_options = options.inject({}) { |hsh, (key, value)| value.nil? ? hsh : hsh.merge(key => value) }
+      needle = arguments.join(" ")
+      Frett::Search.new(search_options).search(needle)
     end
   end
 end
